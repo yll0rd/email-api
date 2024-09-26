6479c1a067ed18cb934e6f999bbc9f84815d668a
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-09-26 05:22:40 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, HTTPException, responses
-from tortoise.contrib.fastapi import register_tortoise, HTTPNotFoundError
+import os
+
+from fastapi import FastAPI, responses
 from pydantic import BaseModel
 from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
 
-from models import EmailReq, Email_Model
+load_dotenv()
 
-app = FastAPI(title="Email-Api")
+app = FastAPI(title="Email-Sender")
 
 # OWN_EMAIL = "elcappicio@gmail.com"
 # OWN_EMAIL_PASSWORD = "123admin#"
-OWN_EMAIL = 'youmbileo14@gmail.com'
-OWN_EMAIL_PASSWORD = 'qrpasmjzbhqrqexh'
+OWN_EMAIL = os.getenv("MAIL_ADDRESS", 'youmbileo14@gmail.com')
+OWN_EMAIL_PASSWORD = os.getenv("MAIL_PASSWORD", 'qrpasmjzbhqrqexh')
 
 
 class EmailBody(BaseModel):
@@ -20,11 +24,13 @@ class EmailBody(BaseModel):
     body: str
 
 
+@app.get("/")
+async def health_check():
+    return "The health check is successful"
+
+
 @app.post("/send_email", response_class=responses.PlainTextResponse)
 async def send_email(em: EmailBody):
-    await EmailReq.create(sender_email=OWN_EMAIL, receiver_email=em.receiver_email, subject=em.subject,
-                          body=em.body)
-    # return await Email_Model.from_tortoise_orm(obj)
     email_client = SMTP("smtp.gmail.com", 587)
     email_client.starttls()
     email_client.login(OWN_EMAIL, OWN_EMAIL_PASSWORD)
@@ -33,18 +39,17 @@ async def send_email(em: EmailBody):
     subject = em.subject
     body = em.body
 
-    message = f"Subject: {subject}\n\n{body}"
+    msg = MIMEMultipart()
+    msg['From'] = OWN_EMAIL
+    msg['To'] = to_address
+    msg['Subject'] = subject
 
-    email_client.sendmail(OWN_EMAIL, to_address, message)
+    # Attach the email body as UTF-8
+    msg.attach(MIMEText(body, 'html', 'utf-8'))
+
+    # message = f"Subject: {subject}\n\n{body}"
+
+    email_client.sendmail(OWN_EMAIL, to_address, msg.as_string())
     email_client.quit()
 
     return "Email sent successfully"
-
-
-register_tortoise(
-    app,
-    db_url="sqlite://db.sqlite3",
-    modules={'models': ['models']},
-    generate_schemas=True,
-    add_exception_handlers=True
-)
